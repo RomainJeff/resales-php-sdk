9eec03cebeed103dcedcac1eb18fe51698371ac0
Romain Quilliot <romain.addweb@gmail.com>
2020-05-03 15:07:18 +0200

    feat: add news features

--- src/Types/Features.php
+++ src/Types/Features.php
@@ -6,7 +6,12 @@ namespace Romainjeff\Resales\Types;
  */
 class Features
 {
-    const CLOSE_TO_GOLF = '1Setting11_2';
-    const CLOSE_TO_SEA  = '1Setting14_2';
-    const PRIVATE_POOL  = '1Pool2_2';
+    const CLOSE_TO_GOLF   = '1Setting11_2';
+    const CLOSE_TO_SEA    = '1Setting14_2';
+    const CLOSE_TO_TOWN   = '1Setting15_2';
+    const PRIVATE_POOL    = '1Pool2_2';
+    const COMMUNAL_POOL   = '1Pool1_2';
+    const SEA_VIEW        = '1Views1_2';
+    const GOLF_VIEW       = '1Views3_2';
+    const PRIVATE_TERRACE = '1Features5_2';
 }

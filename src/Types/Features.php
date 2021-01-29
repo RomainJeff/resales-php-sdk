4d3de3c7e71c5e47271f7d92e598826483fc4c55
Romain Quilliot <romain.addweb@gmail.com>
2021-01-29 17:45:45 +0100

    chore: update resales sdk

--- src/Types/Features.php
+++ src/Types/Features.php
@@ -8,14 +8,27 @@ namespace Romainjeff\Resales\Types;
  */
 class Features
 {
-    const CLOSE_TO_GOLF   = '1Setting11_2';
-    const CLOSE_TO_SEA    = '1Setting14_2';
-    const CLOSE_TO_TOWN   = '1Setting15_2';
-    const PRIVATE_POOL    = '1Pool2_2';
-    const COMMUNAL_POOL   = '1Pool1_2';
-    const SEA_VIEW        = '1Views1_2';
-    const MOUNTAIN_VIEW   = '1Views2_2';
-    const GOLF_VIEW       = '1Views3_2';
-    const PRIVATE_TERRACE = '1Features5_2';
-    const REDUCED         = '1Category10_2';
+    /* API V4 */
+    //const CLOSE_TO_GOLF   = '1Setting11_2';
+    //const CLOSE_TO_SEA    = '1Setting14_2';
+    //const CLOSE_TO_TOWN   = '1Setting15_2';
+    //const PRIVATE_POOL    = '1Pool2_2';
+    //const COMMUNAL_POOL   = '1Pool1_2';
+    //const SEA_VIEW        = '1Views1_2';
+    //const MOUNTAIN_VIEW   = '1Views2_2';
+    //const GOLF_VIEW       = '1Views3_2';
+    //const PRIVATE_TERRACE = '1Features5_2';
+    //const REDUCED         = '1Category10_2';
+
+    /* API V5 */
+    const CLOSE_TO_GOLF   = '1Setting11';
+    const CLOSE_TO_SEA    = '1Setting14';
+    const CLOSE_TO_TOWN   = '1Setting15';
+    const PRIVATE_POOL    = '1Pool2';
+    const COMMUNAL_POOL   = '1Pool1';
+    const SEA_VIEW        = '1Views1';
+    const MOUNTAIN_VIEW   = '1Views2';
+    const GOLF_VIEW       = '1Views3';
+    const PRIVATE_TERRACE = '1Features5';
+    const REDUCED         = '1Category10';
 }

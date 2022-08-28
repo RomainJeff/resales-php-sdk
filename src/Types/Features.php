ab840d03aa58643a61228aa46c07c55ab462f3b5
Romain Quilliot <romain.addweb@gmail.com>
2022-08-28 21:49:20 +0200

    feat: add parking features

--- src/Types/Features.php
+++ src/Types/Features.php
@@ -21,14 +21,17 @@ class Features
     //const REDUCED         = '1Category10_2';
 
     /* API V5 */
-    const CLOSE_TO_GOLF   = '1Setting11';
-    const CLOSE_TO_SEA    = '1Setting14';
-    const CLOSE_TO_TOWN   = '1Setting15';
-    const PRIVATE_POOL    = '1Pool2';
-    const COMMUNAL_POOL   = '1Pool1';
-    const SEA_VIEW        = '1Views1';
-    const MOUNTAIN_VIEW   = '1Views2';
-    const GOLF_VIEW       = '1Views3';
-    const PRIVATE_TERRACE = '1Features5';
-    const REDUCED         = '1Category10';
+    const CLOSE_TO_GOLF         = '1Setting11';
+    const CLOSE_TO_SEA          = '1Setting14';
+    const CLOSE_TO_TOWN         = '1Setting15';
+    const PRIVATE_POOL          = '1Pool2';
+    const COMMUNAL_POOL         = '1Pool1';
+    const SEA_VIEW              = '1Views1';
+    const MOUNTAIN_VIEW         = '1Views2';
+    const GOLF_VIEW             = '1Views3';
+    const PRIVATE_TERRACE       = '1Features5';
+    const REDUCED               = '1Category10';
+    const UNDERGROUND_PARKING   = '1Parking1';
+    const COMMUNAL_PARKING      = '1Parking7';
+    const PRIVATE_PARKING       = '1Parking8';
 }

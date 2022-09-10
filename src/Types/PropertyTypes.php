0eb35a42358c8a39ba140ac23d6c339728f9bf45
Romain Quilliot <romain.addweb@gmail.com>
2022-09-10 15:50:21 +0200

    feat: add duplex property types

--- src/Types/PropertyTypes.php
+++ src/Types/PropertyTypes.php
@@ -9,6 +9,8 @@ class PropertyTypes
     const APARTMENT                 = '1-1';
     const APARTMENT_GROUND_FLOOR    = '1-2';
     const APARTMENT_PENTHOUSE       = '1-6';
+    const APARTMENT_PENTHOUSE_DUPLEX= '1-7';
+    const APARTMENT_DUPLEX          = '1-8';
     const HOUSE                     = '2-1';
     const HOUSE_DETACHED            = '2-2';
     const HOUSE_SEMI_DETACHED       = '2-4';
